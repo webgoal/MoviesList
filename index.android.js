2eeec62bd9e473941429c704b08ffcf9e42e3297
Gustavo Vinícius <gustavovnicius@hotmail.com>
2016-05-23 14:42:34 -0300

    1st step

--- index.android.js
+++ index.android.js
@@ -3,49 +3,48 @@
  * https://github.com/facebook/react-native
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import React, {
+   Component,
+ } from 'react';
+ import {
+   AppRegistry,
+   Image,
+   StyleSheet,
+   Text,
+   View,
+ } from 'react-native';
 
-class MoviesList extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
 });
+
+class MoviesList extends Component {
+  render() {
+    var movie = MOCKED_MOVIES_DATA[0];
+    return (
+      <View style={styles.container}>
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+}
 
 AppRegistry.registerComponent('MoviesList', () => MoviesList);
